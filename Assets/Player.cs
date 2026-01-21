c5cad306551da334962975f6c2accbd4a91012bc
Leonidas Constantinou <leonidasc94.dev@gmail.com>
2026-01-21 23:43:15 +0200

    Initial project files

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public string playerName = "Bob the hero";
+    public int age = 25;
+    public float moveSpeed = 2.5f;
+    public bool gameOver = true;
+    
+    public Rigidbody2D rb;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}

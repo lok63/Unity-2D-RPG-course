85dca417e91f403cdf9c132e5401bfb12e98656f
Leonidas Constantinou <leonidasc94.dev@gmail.com>
2026-01-22 22:40:35 +0200

    Collision detection

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed =5f;
-    [SerializeField] private float jumbForce =17;
     private float xInput;
     private bool isFacingRight = true;
+
+
+    [Header("Movement Details")]
+    [SerializeField] private float moveSpeed =5f;
+    [SerializeField] private float jumbForce =17;
+    
+    [Header("Collision Details")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private bool isGrounded;
+    private LayerMask whatIsGround;
+
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,6 +29,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {   
+        HandleCollision();
         HandleInput();
         HandleMovement();
         HandleAnimations();
@@ -57,7 +69,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
     
     private void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumbForce);
+        // jump only if the character is on the ground
+        // this avoids infinite jumps
+        if (isGrounded)
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumbForce);
     }
-    
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    /**
+     * tranform.position is the center of the character
+     */
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
+    }
 }
